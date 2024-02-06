7a6c468c3771f184cbe926dc3c6658ad7e4dd949
OlyaKhomyn <khomyn2314@gmail.com>
2024-02-06 12:10:07 +0100

    deleted not used actions

--- actions/actions.py
+++ actions/actions.py
@@ -10,30 +10,6 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
 from utils import utils
 
-class ActionChangeOrder(Action):
-	def name(self):
-		return 'action_change_order'
-
-	def run(self, dispatcher, tracker, domain):
-		pizza_size = tracker.get_slot("pizza_size")
-		pizza_type = tracker.get_slot("pizza_type")
-		pizza_amount = tracker.get_slot("pizza_amount")
-		SlotSet("pizza_type", pizza_type)
-		SlotSet("pizza_size", pizza_size)
-		SlotSet("pizza_amount", pizza_amount)
-		return[]
-
-
-class ActionOrderNumber(Action):
-	def name(self):
-		return 'action_order_number'
-
-	def run(self, dispatcher, tracker, domain):
-		name_person = tracker.get_slot("client_name")
-		number_person = tracker.get_slot("phone_number")
-		order_number =  str(name_person + "_"+number_person)
-
-		return[SlotSet("order_number", order_number)]
 
 class ActionUtterConfirm(Action):
 	def name(self):
